03c04b795c53e393b2e4340de67e501f0adb26b7
Steven Tsang <tsangste@gmail.com>
2021-07-24 16:45:30 +0100

    implement categories module

---
+++ src/categories/categories.service.spec.ts
@@ -0,0 +1,64 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+
+import { CategoriesService } from './categories.service';
+import { CategoryDocument } from './entities/category.entity';
+
+describe('CategoriesService', () => {
+  let service: CategoriesService;
+  let model: Model<CategoryDocument>;
+
+  const spyFind = jest.fn();
+  const spyFindOne = jest.fn();
+  const spyUpdateOne = jest.fn();
+  const spyRemove = jest.fn();
+  const mockModel = {
+    find: () => ({ exec: spyFind.mockResolvedValue([]) }),
+    findOne: () => ({ exec: spyFindOne.mockResolvedValue({}) }),
+    updateOne: () => ({ exec: spyUpdateOne.mockResolvedValue({}) }),
+    remove: () => ({ exec: spyRemove.mockResolvedValue({}) }),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CategoriesService,
+        { provide: getModelToken('Category'), useValue: mockModel },
+      ],
+    }).compile();
+
+    service = module.get<CategoriesService>(CategoriesService);
+    model = module.get(getModelToken('Category'));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('findAll', async () => {
+    await service.findAll();
+
+    expect(model.find().exec).toHaveBeenCalled();
+  });
+
+  it('findOne', async () => {
+    await service.findOne('60fc19fc6e92660020ea8653');
+
+    expect(model.findOne().exec).toHaveBeenCalled();
+  });
+
+  it('update', async () => {
+    await service.update('60fc19fc6e92660020ea8653', { description: 'test' });
+
+    expect(model.updateOne().exec).toHaveBeenCalled();
+    expect(model.findOne().exec).toHaveBeenCalled();
+  });
+
+  it('remove', async () => {
+    await service.remove('60fc19fc6e92660020ea8653');
+
+    expect(model.remove().exec).toHaveBeenCalled();
+  });
+});
---
+++ src/categories/categories.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+import { ObjectID } from 'mongodb';
+
+import { Category, CategoryDocument } from './entities/category.entity';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const createdCat = new this.categoryModel(createCategoryDto);
+    return createdCat.save();
+  }
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Category> {
+    return this.categoryModel.findOne({ _id: new ObjectID(id) }).exec();
+  }
+
+  async update(
+    id: string,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    await this.categoryModel
+      .updateOne({ _id: new ObjectID(id) }, updateCategoryDto)
+      .exec();
+
+    return this.categoryModel.findOne({ _id: new ObjectID(id) }).exec();
+  }
+
+  async remove(id: string): Promise<Category> {
+    return this.categoryModel.remove({ _id: new ObjectID(id) }).exec();
+  }
+}

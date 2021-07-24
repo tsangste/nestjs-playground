a478c1a9d9f934860b1949872ae04d2d4e3aa590
Steven Tsang <tsangste@gmail.com>
2021-07-24 16:45:30 +0100

    implement categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://mongo:27017'), CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
